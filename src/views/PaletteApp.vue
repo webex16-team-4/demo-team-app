9e486782ed369f2940943281b950aea6e73a598e
Teruki21 <sano.teruki.r2@dc.tohoku.ac.jp>
2022-11-23 14:01:24 +0900

    paletteの作成をしました

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -1,16 +1,63 @@
 <template>
   <h1>Vue パレット</h1>
   <div class="app">
-    <div class="palette" style="background-color: rgba(0, 0, 200, 0.5)"></div>
-    <p>rgba( {{ 0 }}, {{ 0 }}, 200, 0.5 )</p>
+    <div
+      class="palette"
+      v-on:mousemove="colorpick"
+      v-on:click="colorselect"
+      v-bind:style="paletteStyle"
+    ></div>
+    <p>rgba( {{ red }}, {{ green }}, 200, 0.5 )</p>
     <div class="colors-container">
-      <div class="mini-palette"></div>
+      <div
+        class="mini-palette"
+        v-for="color in colors"
+        v-bind:key="color"
+        v-bind:style="{
+          backgroundColor: `rgba(${color.red}, ${color.green}, 200, 0.5)`,
+        }"
+        v-on:click="showColor(color)"
+      ></div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      colors: [],
+    }
+  },
+  methods: {
+    colorpick: function (e) {
+      this.red = e.offsetX
+      this.green = e.offsetY
+    },
+    colorselect: function () {
+      const SelectedColor = { red: this.red, green: this.green }
+      this.colors.push(SelectedColor)
+    },
+    showColor(color) {
+      this.red = color.red
+      this.green = color.green
+      // console.log(color)
+    },
+  },
+  computed: {
+    paletteStyle() {
+      return {
+        backgroundColor: `rgba(${this.red}, ${this.green}, 200, 0.5)`,
+      }
+    },
+  },
+}
+</script>
+
 <style>
-.app {
+#app {
   display: flex;
   width: 100%;
   flex-direction: column;
