dbaeec29f646b32975232b3b894b9cae947a6b7b
Palm0919 <kishi03100919@gmail.com>
2022-11-25 20:35:04 +0900

    Vueクイズ途中

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,24 +1,46 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ quiz }}</h2>
+    <h2>Q. {{ quizzes[qNum].text }}</h2>
     <img
       class="quiz-image"
       src="https://via.placeholder.com/300x300"
       alt="クイズタイトル"
     />
     <div class="container">
-      <button>
-        {{ "選択肢1" }}
+      <button v-on:click="choose1">
+        {{ quizzes[qNum].choices[0].text }}
       </button>
-      <button>
-        {{ "選択肢2" }}
+      <button v-on:click="choose2">
+        {{ quizzes[qNum].choices[1].text }}
       </button>
-      <button>
-        {{ "選択肢3" }}
+      <button v-on:click="choose3">
+        {{ quizzes[qNum].choices[2].text }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div v-if="answer == 1">
+      <div v-if="quizzes[qNum].choices[0].isCorrect == 1">正解！</div>
+      <div v-if="quizzes[qNum].choices[0].isCorrect == 0">不正解！</div>
+      <div>{{ quizzes[qNum].choices[0].feedback }}</div>
+    </div>
+    <div v-if="answer == 2">
+      <div v-if="quizzes[qNum].choices[1].isCorrect == 1">正解！</div>
+      <div v-if="quizzes[qNum].choices[1].isCorrect == 0">不正解！</div>
+      <div>{{ quizzes[qNum].choices[1].feedback }}</div>
+    </div>
+    <div v-if="answer == 3">
+      <div v-if="quizzes[qNum].choices[2].isCorrect == 1">正解！</div>
+      <div v-if="quizzes[qNum].choices[2].isCorrect == 0">不正解！</div>
+      <div>{{ quizzes[qNum].choices[2].feedback }}</div>
+    </div>
+
+    <div v-if="nextQuiz">
+      <p></p>
+      <button v-on:click="nextQ">次の問題へ</button>
+    </div>
+    <div>{{ qNum }}</div>
+
+    <!-- <div>{{ "答え" }}</div> -->
   </div>
 </template>
 
@@ -26,28 +48,60 @@
 export default {
   data() {
     return {
-      quiz: {
-        text:
-        image:
-        choices: [
-          {
-            text:
-            isCorrect:
-            feedback:
-          },
-          {
-            text:
-            isCorrect:
-            feedback:
-          },
-          {
-            text:
-            isCorrect:
-            feedback:
-          },
-        ]
-      }
+      qNum: 0,
+      answer: 0,
+      nextQuiz: false,
+      quizzes: [
+        //1問目
+        {
+          text: "1問目",
+          // image:
+          choices: [
+            {
+              text: "1",
+              isCorrect: 1,
+              feedback: "1feed",
+            },
+            {
+              text: "2",
+              isCorrect: 0,
+              feedback: "2feed",
+            },
+            {
+              text: "3",
+              isCorrect: 0,
+              feedback: "3feed",
+            },
+          ],
+        },
+      ],
     }
+  },
+  methods: {
+    choose1: function () {
+      this.answer = 1
+      if (this.quizzes[this.qNum].choices[0].isCorrect == 1) {
+        this.nextQuiz = true
+      }
+    },
+    choose2: function () {
+      this.answer = 2
+      if (this.quizzes[this.qNum].choices[1].isCorrect == 1) {
+        this.nextQuiz = false
+      }
+    },
+    choose3: function () {
+      this.answer = 3
+      if (this.quizzes[this.qNum].choices[2].isCorrect == 1) {
+        this.nextQuiz = false
+      }
+    },
+
+    nextQ: function () {
+      this.qNum += 1
+      this.answer = 0
+      this.nextQuiz = false
+    },
   },
 }
 </script>
